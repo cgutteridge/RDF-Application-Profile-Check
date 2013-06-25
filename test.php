446f05e64445798ffbca45913c68bf689d344a5c
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2013-06-25 15:35:18 +0100

    stage

--- test.php
+++ test.php
@@ -3,12 +3,17 @@
 require_once( "../arc2-fork/ARC2.php" );
 require_once( "../Graphite/graphite/Graphite.php" );
 
+$doc_url = "soton-opd.ttl";
+$profile_url = "opd-profile.ttl";
+
 $graph = new Graphite();
 $graph->setDebug( true );
 
-$profile_ttl = file_get_contents( "profile.ttl" );
-print $profile_ttl."\n";
-$n = $graph->addTurtle( "http://id.southampton.ac.uk/", $profile_ttl );
+$doc_ttl = file_get_contents( $doc_url );
+$n = $graph->addTurtle( $doc_url, $doc_ttl );
+print "Doc N Triples= $n\n";
 
-print "N Triples= $n\n";
-print $graph->dumpText();
+$profile_ttl = file_get_contents( "profile.url" );
+$n = $graph->addTurtle( $profile_url, $profile_ttl );
+print "Profile N Triples= $n\n";
+
