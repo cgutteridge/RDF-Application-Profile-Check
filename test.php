0d21c514d0f34549449bdba28abbb5d08cdeb188
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2013-07-18 11:28:52 +0100

    refactor

--- test.php
+++ test.php
@@ -6,19 +6,103 @@ require_once( "../Graphite/Graphite.php" );
 $doc_url = "soton-opd.ttl";
 $profile_url = "opd-profile.ttl";
 
-$doc_g = new Graphite();
-$doc_g->setDebug( true );
+$ap = new AppProfile( $profile_url, new AppProfileIOCmdLine() );
+$ap->applyTemplates( $doc_url );
+exit;
 
-$doc_ttl = file_get_contents( $doc_url );
-$n = $doc_g->addTurtle( $doc_url, $doc_ttl );
-print "Doc N Triples= $n\n";
+class AppProfileIOHTML
+{
+	function apError( $msg )
+	{
+		print "<h1>Error in Application Profile</h1>\n";
+		print "<p>$msg</p>\n";
+	}
 
-$ap_g = new Graphite();
-$ap_g->setDebug( true );
-$profile_ttl = file_get_contents( $profile_url );
-$ap_g->ns( "dsp","http://purl.org/dc/dsp/" );
-$n = $ap_g->addTurtle( $profile_url, $profile_ttl );
-print "Profile N Triples= $n\n";
+	function docError( $msg )
+	{
+		print "<h1>Error in Document</h1>\n";
+		print "<p>$msg</p>\n";
+	}
+
+	function message( $msg )
+	{
+		print "<p>$msg</p>\n";
+	}
+
+	function prettyLink( $resource )
+	{
+		return $resource->prettyLink();
+	}
+
+	function incDepth()
+	{
+		print "<div style='margin-left: 2em'>";
+	}
+
+	function decDepth()
+	{
+		print "</div>";
+	}
+}
+class AppProfileIOCmdLine
+{
+	var $depth = 0;
+	function apError( $msg )
+	{
+		$this->message( "ERROR (PROFILE): $msg" );
+	}
+
+	function docError( $msg )
+	{
+		$this->message( "ERROR (DOCUMENT): $msg" );
+	}
+
+	function message( $msg )
+	{
+		if( $this->depth ) { for( $i=0;$i<=$this->depth;++$i ) { print "  "; } }
+		print "* $msg\n";
+	}
+
+	function prettyLink( $resource )
+	{
+		if( $resource->hasLabel() )
+		{
+			return "\"".$resource->label()."\"";
+		}
+		return "<". $resource->toString().">";
+	}
+
+	function incDepth() { $this->depth++; }
+	function decDepth() { $this->depth--; }
+}
+
+class AppProfile {
+
+var $ap_g;
+var $io;
+function __construct( $profile_url, $io=null )
+{
+	if( $io === null ) { $io = new AppProfileIOCmdLine(); }
+	$this->io = $io;
+	$this->ap_g = new Graphite();
+	$this->ap_g->setDebug( true );
+
+	$this->io->message( "Loading profile: $profile_url" );
+	$profile_ttl = file_get_contents( $profile_url );
+	$this->ap_g->ns( "dsp", "http://purl.org/dc/dsp/" );
+	$n = $this->ap_g->addTurtle( $profile_url, $profile_ttl );
+	$this->io->message( "Profile N Triples= $n" );
+}
+
+function applyTemplates( $doc_url )
+{
+	$doc_g = new Graphite();
+	$doc_g->setDebug( true );
+	
+	$this->io->message( "Loading document: $doc_url" );
+	$doc_ttl = file_get_contents( $doc_url );
+	$n = $doc_g->addTurtle( $doc_url, $doc_ttl );
+	$this->io->message( "Doc N Triples= $n" );
 
 # TODO: check each desc-template is only applied once per URI
 # TODO: make $resource->all() do de-dup. (make default to do this, but option to disable for speed)
@@ -32,59 +116,84 @@ print "Profile N Triples= $n\n";
   # for all resources which match
     # check constraints
     # check this template has not already been applied to this resource
-    # report 
+    	# report 
     # apply X
-foreach( $ap_g->allOfType( "dsp:DescriptionTemplate" ) as $description_t )
-{
-	if( $description_t->has( "dsp:standalone" ) 
-	 && $description_t->getString( "dsp:standalone" ) == "false" ) { continue; }
-	print "//start of loop\n";
-	print "standalone!\n";
-	print "DESCR:\n". $description_t->dumpText();
+	$standAloneTemplateCount = 0;
 
-	if( !$description_t->has( "dsp:resourceClass" ) )
+	foreach( $this->ap_g->allOfType( "dsp:DescriptionTemplate" ) as $description_t )
 	{
-		ap_error( $description_t->prettyLink()." has no dsp:resourceClass" );
+		if( $description_t->has( "dsp:standalone" ) 
+	 	&& $description_t->getString( "dsp:standalone" ) == "false" ) { continue; }
+		
+		$standAloneTemplateCount++;
+
+		$desc = $this->io->prettyLink( $description_t );
+		$min_max = $this->describeMinMax( $description_t );
+	
+		if( !$description_t->has( "dsp:resourceClass" ) )
+		{
+			$this->io->apError( $desc." has no dsp:resourceClass" );
+			continue;
+		}
+		$classes = $description_t->all( "dsp:resourceClass" );
+		if( sizeof($classes) == 1 )
+		{
+			$class_desc = $this->io->prettyLink( $classes[0] );
+		}
+		else
+		{
+			$a = array();
+			foreach( $class_desc as $class ) 
+			{
+				$a[]= $this->io->prettyLink( $class );
+			}
+			$class_desc = "( ".join( ", ", $a )." )";
+		}
+		$this->io->message( "Applying stand-alone template $desc which requires $min_max instance(s) of class(es) ".$class_desc." in the target document." );
+		$this->io->incDepth();
+	
+		$matches = new Graphite_ResourceList($doc_g);
+		foreach( $classes as $class )
+		{
+			$more_matches = $doc_g->allOfType( $class->toString() );
+			$matches = $matches->append( $more_matches );
+		}
+		$matches = $matches->distinct();
+	
+		if( $description_t->has( "dsp:minOccur" ) && sizeof( $matches ) < $description_t->getString( "dsp:minOccur" ) )
+		{
+			$this->io->docError( "document has ".(sizeof($matches))." matching instance(s) but should have $min_max" );
+		}
+
+		elseif( $description_t->has( "dsp:maxOccur" ) && sizeof( $matches ) > $description_t->getString( "dsp:maxOccur" ) )
+		{
+			$this->io->docError( "document has ".(sizeof($matches))." matching instance(s) but should have $min_max" );
+		}
+		else
+		{
+			$this->io->message( "document has ".(sizeof($matches))." matching instance(s) which is legit.");
+		}
+
+		foreach( $matches as $match )
+		{
+			$this->applyClassTemplate( $description_t, $match );
+		}
+
+		$this->io->decDepth();
 	}
 
-	$classes = $description_t->all( "dsp:resourceClass" );
-	print "CLASSES:\n". $classes->dumpText();
-#$classes = $doc_g->resource( "foaf:Organization" );
-print "*******\n";
-	$matches = $doc_g->allOfType( "foaf:Organization" );
-	print "MATCHES:\n". $matches->dumpText();
-
-	if( $description_t->has( "dsp:minOccur" ) && sizeof( $matches ) < $description_t->getString( "dsp:minOccur" ) )
+	if( $standAloneTemplateCount == 0 )
 	{
-		ap_error( $description_t->prettyLink()." -- document has ".(sizeof($matches))." but should have at least ".$description_t->getString( "dsp:minOccur" )." and at most ".$description_t->getString( "dsp:maxOccur" ));
+		$this->io->apError( "Did not find any standalone templates." );
 	}
-
-	if( $description_t->has( "dsp:maxOccur" ) && sizeof( $matches ) > $description_t->getString( "dsp:maxOccur" ) )
+	else
 	{
-		ap_error( $description_t->prettyLink()." -- document has ".(sizeof($matches))." but should have at least ".$description_t->getString( "dsp:minOccur" )." and at most ".$description_t->getString( "dsp:maxOccur" ) );
+		$this->io->message( "Applied $standAloneTemplateCount stand alone (top level) template(s)." );
 	}
-	print "//end of loop\n";
-}
-exit;
-
-function ap_error( $msg )
-{
-	print "\n";
-	print "<h1>Error in Application Profile</h1>\n";
-	print "<p>$msg</p>\n";
-	exit;
-}
-
-function doc_error( $msg )
-{
-	print "\n";
-	print "<h1>Error in Document</h1>\n";
-	print "<p>$msg</p>\n";
-	exit;
 }
 
 
-#X:
+#apply-template:
   # report 
   # foreach statement templates & inverse statement template
     # check constraints
@@ -99,3 +208,48 @@ function doc_error( $msg )
         # check this template has not already been applied to this resource
         # X 
 # unused triples report?
+
+function applyClassTemplate( $template, $resource )
+{
+	$t_desc = $this->io->prettyLink( $template );
+	$r_desc = $this->io->prettyLink( $resource );
+
+	print "in act\n";	
+	$this->io->message( "Applying class template $t_desc to resource $r_desc" );
+	print $template->dumpText()."\n";
+	foreach( $template->all( "dsp:statementTemplate" ) as $s_template )
+	{
+		print "A--\n";
+var_dump( $s_template );
+		print $s_template."\n";
+		print $s_template->dumpText()."\n";
+		print "B--\n";
+	}
+	print "out act\n";	
+}
+
+// return a string describing the minimum and maximum occurances allowed
+// assumes minOccur or maxOccur is set.
+function describeMinMax( $description_t )
+{
+	if( $description_t->has( "dsp:minOccur" ) && !$description_t->has( "dsp:maxOccur" ) )
+	{
+		return "at least ".$description_t->getString( "dsp:minOccur" );
+	}
+
+	if( !$description_t->has( "dsp:minOccur" ) && $description_t->has( "dsp:maxOccur" ) )
+	{
+		return "at most ".$description_t->getString( "dsp:minOccur" );
+	}
+
+	$min = $description_t->getString( "dsp:minOccur" );	
+	$max = $description_t->getString( "dsp:maxOccur" );
+	if( $min == $max )
+	{
+		return "exactly $min";
+	}
+			
+	return "at least $min and at most $max";
+}
+
+} // end of class AppProfile
